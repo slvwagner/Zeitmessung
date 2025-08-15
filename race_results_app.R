0c7c509d21e7be45c4ceb207a5408ffdd6661def
Florian Wagner <53572950+slvwagner@users.noreply.github.com>
2025-08-15 20:28:30 +0200

    filter recovery

--- race_results_app.R
+++ race_results_app.R
@@ -25,6 +25,22 @@ DT_language <- list(
   )
 )
 
+# get date type for each column from a data frame ####
+get_data_type <- function(df){
+  1:ncol(df)|>
+    lapply(function(x){
+      c_temp <- df|>
+        select(all_of(x))|>
+        pull()
+      class(c_temp)[1] # only use the first class
+    })|>
+    unlist()
+}
+
+# Escape regex literals ####
+escape_regex <- function(pattern) {
+  gsub("([][{}()+*^$|\\\\?.])", "\\\\\\1", pattern)
+}
 
 # DB connection function ####
 DB_connect <- function(DB_host, DB_name, DB_user, DB_PW = NULL, max_attempts = 3) {
@@ -76,7 +92,9 @@ server <- function(input, output, session) {
   con <- DB_connect("wagnius", "zeitmessung", "race", "49rb61")
   
   ## reactive values ####
+  ### Filters race results #### 
   last_user_filter <- shiny::reactiveVal(NULL)
+  current_data <- shiny::reactiveVal(NULL)
   
   ## Poll DB for Results ####
   race_data <- reactivePoll(
@@ -118,7 +136,7 @@ server <- function(input, output, session) {
         }
       })
       
-      bind_rows(l_results)
+      return(bind_rows(l_results))
     }
   )
   
@@ -170,8 +188,21 @@ server <- function(input, output, session) {
   
   # Render in results table ####
   output$race_results <- renderDT({
+    
+    df_temp <-race_data()|>
+      arrange(Zeit)|>
+      mutate(Rang = row_number(),
+             # Stunden = lubridate::hour(Zeit),
+             # Minuten = lubridate::minute(Zeit),
+             # Sekunden = lubridate::second(Zeit),
+             Zeit = paste0(as.character(round(Zeit,3)), " Sekunden")
+             )|>
+      select(Rang, Startnummer, Zeit)
+    
+    current_data(df_temp)
+    
     datatable(
-      race_data(), 
+      df_temp, 
       rownames = FALSE,
       filter = "top",
       options = 
@@ -186,6 +217,85 @@ server <- function(input, output, session) {
       )
   })
   
+  
+  ## check if last user filter has been cleared ####
+  observeEvent(input$race_results_search_columns,{
+    
+    df_temp <- current_data()
+    
+    # get user filters
+    column_filters = input$race_results_search_columns
+    column_filters <- column_filters|>
+      str_remove_all("\"")|>
+      str_remove_all("\\[")|>
+      str_remove_all("\\]")
+    column_filters <- str_split(column_filters,",")
+    
+    # Update last user filter
+    c_test <- lapply(column_filters, function(x){
+      nchar(x) > 0
+    })|>
+      unlist()
+    
+    # get column data type
+    c_class <- get_data_type(df_temp)
+    
+    ### extract data from column filters ####
+    for (ii in 1:length(column_filters)) {
+      col_filter <- column_filters[[ii]]
+      if(nchar(col_filter[1]) > 0){
+        if(c_class[ii] %in% c("Date", "hms")){
+          c_date <- pull(df_temp[,ii])|>
+            as.character()
+          df_temp <- df_temp[str_detect(c_date, col_filter),]
+          df_temp <- df_temp[!is.na(pull(df_temp[,ii])),]
+        } 
+        else if(c_class[ii] == "integer"){
+          p1 <- "^[\\d]+"
+          p2 <- "[\\d]+$"
+          start <- str_extract(col_filter, p1)|>
+            as.integer()
+          end <- str_extract(col_filter, p2)|>
+            as.integer()
+          c_select <- start:end
+          df_temp <- df_temp[pull(df_temp[,ii]) %in% c_select,]
+        } else if (c_class[ii] == "factor"){
+          if(length(col_filter) > 1){
+            df_temp <- df_temp[pull(df_temp[,ii]) %in% col_filter,] 
+          } else {
+            c_select <- str_detect(pull(df_temp[,ii]), col_filter)
+            c_select <- ifelse(is.na(c_select), FALSE, c_select)
+            df_temp <- df_temp[c_select,]
+          }
+        }
+        # character 
+        else { 
+          col_filter <- col_filter|>
+            tolower()|>
+            escape_regex()
+          
+          df_temp <- df_temp[str_detect(pull(df_temp[,ii])|>tolower(), col_filter),] 
+          df_temp <- df_temp[!is.na(pull(df_temp[,ii])),]
+        }
+      }
+    }
+    
+    ### if column filters are present update column filters #####
+    if(sum(!c_test) != length(column_filters)) {
+      column_filters_temp <- input$race_results_search_columns|>
+        lapply(function(x){
+          if(nchar(x) > 0) {
+            list(search = x)
+          } 
+          else {
+            NULL
+          }
+        })
+      last_user_filter(column_filters_temp)
+    } else {
+      last_user_filter(NULL)
+    }
+  })
   
   
   # Close connection when session ends ####
