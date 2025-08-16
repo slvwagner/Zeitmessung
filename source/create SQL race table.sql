8755f630f4adc4e43fa19f4d45752d5739bb796b
Florian Wagner <53572950+slvwagner@users.noreply.github.com>
2025-08-16 18:17:03 +0200

    mc

--- source/create SQL race table.sql
+++ source/create SQL race table.sql
@@ -1,7 +1,6 @@
--- !preview conn=DBI::dbConnect(RSQLite::SQLite())
 CREATE TABLE race (
     id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    value VARCHAR(100) NOT NULL,
+    value TEXT NOT NULL,
     timestamp DATETIME(3) NOT NULL,
     device_id VARCHAR(32) NOT NULL,
     device_name VARCHAR(50) NOT NULL,
@@ -12,8 +11,6 @@ CREATE TABLE race (
     Phone VARCHAR(50) DEFAULT '',
     `E-mail` VARCHAR(150) DEFAULT '',
     last_updated DATETIME(3) NOT NULL,
-    INDEX idx_device_id (device_id),
     INDEX idx_race_status (race_status),
     INDEX idx_timestamp (timestamp)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
