0564a74f28ea631852a04b369604c8d5e6ae4028
Florian Wagner <slvwagner@gmail.com>
2025-08-16 18:35:56 +0200

    credentials template added

---
+++ credentials_template.py
@@ -0,0 +1,9 @@
+# --- WiFi credentials ---
+SSID = ""
+PASSWORD = ""
+
+# --- Configuration ---
+SERVER_URL = "http://nadias_laptop/insert.php"
+READ_URL = "http://nadias_laptop/read.php"
+EDIT_URL = "http://nadias_laptop/edit.php"
+TIMEZONE_OFFSET = 2  # UTC+2
