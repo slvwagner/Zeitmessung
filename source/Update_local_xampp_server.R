4a136abd7cdc253567be94be4902c79c891e9401
Florian Wagner <53572950+slvwagner@users.noreply.github.com>
2025-08-16 13:47:21 +0200

    Update php upload to local xampp server

---
+++ source/Update_local_xampp_server.R
@@ -0,0 +1,17 @@
+# Script to update htdocs in local xampp server
+
+c_xampp_path <- "D:/xampp/htdocs"
+
+c_files <- list.files(path = "source/PHP_Xampp",pattern = "php")
+c_files
+
+c_filePath <- list.files(pattern = "php", recursive = TRUE)
+c_filePath
+
+for (ii in 1:length(c_files)) {
+  c_raw <- readLines(c_filePath[ii])|>
+    suppressWarnings()
+  writeLines(c_raw, paste0(c_xampp_path,"/",c_files[ii]))
+}
+
+message("The following files: ", paste(".../",c_filePath, collapse = ", ")," have been written to ", c_xampp_path)
