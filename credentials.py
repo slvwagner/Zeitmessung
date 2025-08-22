adcca7e0df01a3f1e05500dcf30c2ffa01e83a57
Florian Wagner <53572950+slvwagner@users.noreply.github.com>
2025-08-22 16:39:11 +0200

    mc

---
+++ credentials.py
@@ -0,0 +1,18 @@
+# --- WiFi credentials ---
+SSID = "WN-888F40"
+PASSWORD = "pdn8f428vk"
+
+# --- Configuration ---
+SERVER_URL = "http://192.168.0.17/insert.php"
+READ_URL = "http://192.168.0.17/read.php"
+EDIT_URL = "http://192.168.0.17/edit.php"
+last_startnummer = "https://wagnius/get_last_startnummer.php"
+TIMEZONE_OFFSET = 0  # UTC+2
+
+# --- SQL Server ---
+DB_host = "http://192.168.0.17/insert.php";
+DB_username = "race";
+DB_password = "49rb61";
+DB_name = "zeitmessung";
+
+
