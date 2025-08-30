ea2653a7eba87e8e1f807510478dfb93b47ee5ae
Florian Wagner <53572950+slvwagner@users.noreply.github.com>
2025-08-30 22:17:51 +0200

    mc

--- source/PHP_Xampp/read.php
+++ source/PHP_Xampp/read.php
@@ -3,7 +3,7 @@
 header('Content-Type: application/json; charset=UTF-8');
 
 // === DB CONFIG ===
-$DB_HOST = '127.0.0.1';
+$DB_HOST = 'localhost';
 $DB_NAME = 'zeitmessung_V2';
 $DB_USER = 'root';
 $DB_PASS = '';
