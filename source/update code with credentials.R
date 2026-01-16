b8ae97582d8a558fa9c2003263c6ff0d35e7195e
Florian Wagner <slvwagner@gmail.com>
2026-01-16 16:12:46 +0100

    New function to update ip adresse for credentials

--- source/update code with credentials.R
+++ source/update code with credentials.R
@@ -1,5 +1,31 @@
 library(tidyverse)
 
+# ------------------------------------------------------------
+# Get host IPv4 address (cross-platform)
+# ------------------------------------------------------------
+get_host_ipv4 <- function() {
+  os <- Sys.info()[["sysname"]]
+  
+  if (os == "Windows") {
+    ip_raw <- system("ipconfig", intern = TRUE)
+    
+    # Windows output contains "IPv4 Address"
+    ip_raw |>
+      str_subset("IPv4") |>
+      str_extract("\\d+\\.\\d+\\.\\d+\\.\\d+") |>
+      first()
+    
+  } else {
+    # Linux / Ubuntu / macOS
+    ip_raw <- system("ip -4 addr show", intern = TRUE)
+    
+    ip_raw |>
+      str_extract("(?<=inet\\s)\\d+(\\.\\d+){3}") |>
+      na.omit() |>
+      first()
+  }
+}
+
 # get host server IP
 ip <- system("ipconfig", intern = TRUE)
 
