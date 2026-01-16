e103e0196828a0ddc968002e8b76d06fb64b87dc
Florian Wagner <slvwagner@gmail.com>
2026-01-16 16:10:13 +0100

    Function to update IP adress

--- source/update code with credentials.R
+++ source/update code with credentials.R
@@ -1,5 +1,42 @@
 library(tidyverse)
 
+library(tidyverse)
+
+# ------------------------------------------------------------
+# Get host IPv4 address (cross-platform)
+# ------------------------------------------------------------
+get_host_ipv4 <- function() {
+  os <- Sys.info()[["sysname"]]
+  
+  if (os == "Windows") {
+    ip_raw <- system("ipconfig", intern = TRUE)
+    
+    # Windows output contains "IPv4 Address"
+    ip_raw |>
+      str_subset("IPv4") |>
+      str_extract("\\d+\\.\\d+\\.\\d+\\.\\d+") |>
+      first()
+    
+  } else {
+    # Linux / Ubuntu / macOS
+    ip_raw <- system("ip -4 addr show", intern = TRUE)
+    
+    ip_raw |>
+      str_extract("(?<=inet\\s)\\d+(\\.\\d+){3}") |>
+      na.omit() |>
+      first()
+  }
+}
+
+DB_hoste_name <- get_host_ipv4()
+
+if (is.na(DB_hoste_name)) {
+  stop("Could not determine host IPv4 address")
+}
+
+DB_hoste_name
+
+
 # get host server IP
 ip <- system("ipconfig", intern = TRUE)
 
@@ -14,7 +51,7 @@ DB_hoste_name <- ip[str_detect(ip,"IPv4")][1]|>
 DB_hoste_name
 
 # get credentials
-c_file <- "credentials.py"
+c_file <- "credentials_template.py"
 c_raw <- readLines(c_file)
 
 c_new <- str_replace_all(
