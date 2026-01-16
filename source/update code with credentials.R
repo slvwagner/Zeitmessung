35c59af63d2be93516995d17b274a58fedeeffea
Florian Wagner <slvwagner@gmail.com>
2026-01-16 17:28:53 +0100

    Update credentials with values from system variables

--- source/update code with credentials.R
+++ source/update code with credentials.R
@@ -18,49 +18,64 @@ get_host_ipv4 <- function() {
   } else {
     # Linux / Ubuntu / macOS
     ip_raw <- system("ip -4 addr show", intern = TRUE)
-    print(ip_raw)
+    re_ipv4_leading_zeros_in_text <- "(?<!\\d)(?:25[0-5]|2[0-4]\\d|1\\d\\d|0?\\d?\\d)(?:\\.(?:25[0-5]|2[0-4]\\d|1\\d\\d|0?\\d?\\d)){3}(?!\\d)"
     
     ip_raw[6:length(ip_raw)] |>
-      str_extract("(?<=inet\\s)\\d+(\\.\\d+){3}") |>
+      str_extract(re_ipv4_leading_zeros_in_text) |>
       na.omit() |>
       first()
   }
 }
 
-
 # get host server IP
-# ip <- system("ipconfig", intern = TRUE)
-
-ip <- get_host_ipv4()
-
-re_ipv4_leading_zeros_in_text <- "(?<!\\d)(?:25[0-5]|2[0-4]\\d|1\\d\\d|0?\\d?\\d)(?:\\.(?:25[0-5]|2[0-4]\\d|1\\d\\d|0?\\d?\\d)){3}(?!\\d)"
-
-ip[str_detect(ip,"IPv4")][1]
-
-
-DB_hoste_name <- ip[str_detect(ip,"IPv4")][1]|>
-  str_extract(re_ipv4_leading_zeros_in_text)
-
+DB_hoste_name <- get_host_ipv4()
 DB_hoste_name
 
 # get credentials
 c_file <- "credentials.py"
 
+if(length(list.files(pattern = ".py")) == 0) {
+  warning("File: credentials.py cannot be found")
+  warning("A new credentials.py will be created from credentials_template.py")
+  file.copy("source/credentials_template.py", c_file)
+}
 
-c_files <- list.files(path = "source",pattern = ".py", full.names = TRUE)
-if(sum(c_files == c_file) < length(c_files)) stop("File: credentials.py cannot be found")
-
-
-# get credentials
-c_file <- "credentials.py"
+# get file with credentials
 c_raw <- readLines(c_file)
+c_raw
 
+# update host ip
 c_new <- str_replace_all(
   c_raw,
   # regex for IPv4 inside http://
   "http://[0-9\\.]+",
   paste0("http://", DB_hoste_name))
 c_new
+
+# get wifi connection
+SSID <- Sys.getenv("SSID")
+if(str_length(SSID)>0){
+  c_new[str_detect(c_new,"SSID")] <- paste0("SSID = ", SSID)
+}else{
+  warning("API_KEY not found in system variables")
+}
+
+SSID_PW <- Sys.getenv("SSID_PW")
+if(str_length(SSID_PW)>0){
+  c_new[str_detect(c_new,"PASSWORD")] <- paste0("PASSWORD = ", SSID_PW)
+}else{
+  warning("API_KEY not found in system variables")
+}
+
+
+API_KEY <- Sys.getenv("API_KEY")
+if(str_length(API_KEY)>0){
+  c_new[str_detect(c_new,"API_KEY")] <- paste0("API_KEY = ", API_KEY)
+}else{
+  warning("API_KEY not found in system variables")
+}
+
+
 
 writeLines(c_new)
 # write updated file
