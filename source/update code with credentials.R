6c7c1aaa6d268574c92b54f58113bf5fc16029e6
Florian Wagner <slvwagner@gmail.com>
2026-01-27 11:35:47 +0100

    reworked credential file creation

--- source/update code with credentials.R
+++ source/update code with credentials.R
@@ -34,11 +34,9 @@ DB_hoste_name
 # get credentials
 c_file <- "credentials.py"
 
-if(length(list.files(pattern = ".py")) == 0) {
-  warning("File: credentials.py cannot be found")
-  warning("A new credentials.py will be created from credentials_template.py")
-  file.copy("source/credentials_template.py", c_file)
-}
+warning("A new credentials.py file has been created form credentials_template.py")
+file.copy("source/credentials_template.py", c_file, overwrite = TRUE)
+
 
 # get file with credentials
 c_raw <- readLines(c_file)
@@ -49,20 +47,21 @@ c_new <- str_replace_all(
   c_raw,
   # regex for IPv4 inside http://
   "http://[0-9\\.]+",
-  paste0("http://", DB_hoste_name))
+  paste0("http://", DB_hoste_name,"/zeitmessung/xampp"))
 c_new
 
 # get wifi connection
 SSID <- Sys.getenv("SSID")
 if(str_length(SSID)>0){
-  c_new[str_detect(c_new,"SSID")] <- paste0("SSID = ", SSID)
+  c_new[str_detect(c_new,"SSID")] <- paste0("SSID = \"", SSID, "\"")
 }else{
   warning("API_KEY not found in system variables")
 }
 
+# Get Wifi connection details
 SSID_PW <- Sys.getenv("SSID_PW")
 if(str_length(SSID_PW)>0){
-  c_new[str_detect(c_new,"PASSWORD")] <- paste0("PASSWORD = ", SSID_PW)
+  c_new[str_detect(c_new,"PASSWORD")] <- paste0("PASSWORD = \"", SSID_PW, "\"")
 }else{
   warning("API_KEY not found in system variables")
 }
@@ -70,11 +69,24 @@ if(str_length(SSID_PW)>0){
 # Get API key to secure the communication
 API_KEY <- Sys.getenv("API_KEY")
 if(str_length(API_KEY)>0){
-  c_new[str_detect(c_new,"API_KEY")] <- paste0("API_KEY = ", API_KEY)
+  c_new[str_detect(c_new,"API_KEY")] <- paste0("API_KEY = \"", API_KEY, "\"")
 }else{
   warning("API_KEY not found in system variables")
 }
 
+# The path to local xampp server and its subdirectory for this project must be defined in environment variable
+c_xampp_path <- chartr("\\", "/", Sys.getenv("xampp_server"))
+c_xampp_path 
+
+if (c_xampp_path == "") {
+  stop("Environment variable 'xampp_server' is not set")
+}
+
+if (file.access(c_xampp_path, 2) != 0) {
+  stop("No write permission to: ", c_xampp_path)
+}
+
+c_xampp_path
 
 
 writeLines(c_new)
