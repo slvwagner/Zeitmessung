0ec49d784adab590d0e59e3ccb528ebdbb32ed93
Florian Wagner <slvwagner@gmail.com>
2026-01-27 11:02:01 +0100

    Update for directory structure rework

--- source/Update_local_xampp_server.R
+++ source/Update_local_xampp_server.R
@@ -1,7 +1,11 @@
 # Script to update htdocs in local xampp server
+library(tidyverse)
+library(rebus)
+slvwagner::r_path
 
-# The path to local xampp server must be defined in environment variable
-c_xampp_path <- Sys.getenv("xampp_server")
+# The path to local xampp server and its subdirectory for this project must be defined in environment variable
+c_xampp_path <- chartr("\\", "/", Sys.getenv("xampp_server"))
+c_xampp_path
 
 if (c_xampp_path == "") {
   stop("Environment variable 'xampp_server' is not set")
@@ -11,33 +15,38 @@ if (file.access(c_xampp_path, 2) != 0) {
   stop("No write permission to: ", c_xampp_path)
 }
 
+# Source and xampp server sub directories 
+c_paths <- c("www_check_registrations", "www_register","xampp")
+c_paths
 
-# get php 
-c_files <- list.files(path = "source/XAMPP",pattern = "php")
-c_files
+# Unlink or delete old data on xampp server for this project
+unlink(c_xampp_path, recursive = TRUE)
 
-c_filePath <- list.files(pattern = "php", recursive = TRUE)
-c_filePath
+# Recreate the project directory
+dir.create(c_xampp_path)
 
-for (ii in 1:length(c_files)) {
-  c_raw <- readLines(c_filePath[ii])
-  writeLines(c_raw, paste0(c_xampp_path,"/",c_files[ii]))
+# Recreate the sub directory structure
+c_xampp_subdirectories <- (paste0(c_xampp_path, "/", c_paths))
+c_xampp_subdirectories
+
+for (ii in 1:length(c_xampp_subdirectories)) {
+  dir.create(c_xampp_subdirectories[ii])  
 }
 
-message("The following files:\n", paste0(".../",c_filePath, collapse = "\n"),"\nhave been written to ", c_xampp_path)
+# Source files 
+source_files <- list.files(path = paste0( "source/",c_paths), full.names = TRUE)
+source_files
 
-# get html
-c_files <- list.files(path = "source/XAMPP",pattern = "html")
-c_files
+# xampp server files
+xampp_files <- str_remove(source_files, "source/")
+xampp_files <- paste0(c_xampp_path,"/", xampp_files)
+xampp_files
 
-c_filePath <- list.files(pattern = "html", recursive = TRUE)
-c_filePath
+# Copy data 
+for (ii in 1:length(source_files)) {
+  file.copy(source_files, xampp_files)
 
-ii <- 2
-for (ii in 1:length(c_files)) {
-  c_raw <- readLines(c_filePath[ii])
-  c_raw
-  writeLines(c_raw, paste0(c_xampp_path,"/",c_files[ii]))
 }
 
-message("The following files:\n", paste0(".../",c_filePath, collapse = "\n"),"\nhave been written to ", c_xampp_path)
+message("The following files:\n", paste0(".../",source_files, collapse = "\n"),"\nhave been written to ", paste0(xampp_files, collapse = "\n"))
+
