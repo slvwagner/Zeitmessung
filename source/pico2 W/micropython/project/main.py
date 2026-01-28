2cb06612bc3106e15fea4d65c6811f4eea54c686
Florian Wagner <slvwagner@gmail.com>
2026-01-28 21:50:09 +0100

    micropython project created

---
+++ source/pico2 W/micropython/project/main.py
@@ -0,0 +1,17 @@
+# MicroPython main.py
+# Write your code here
+
+from machine import Pin
+from utime import sleep
+
+pin = Pin("LED", Pin.OUT)
+
+print("LED starts flashing...")
+while True:
+    try:
+        pin.toggle()
+        sleep(1) # sleep 1sec
+    except KeyboardInterrupt:
+        break
+pin.off()
+print("Finished.")
