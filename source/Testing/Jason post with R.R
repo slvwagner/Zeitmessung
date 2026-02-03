842d8d377fbd79b975d6352e5152f789ba609924
Florian Wagner <slvwagner@gmail.com>
2026-02-03 11:30:18 +0100

    mc

--- source/Testing/Jason post with R.R
+++ source/Testing/Jason post with R.R
@@ -20,7 +20,7 @@ content(response, "text")
 status_code(response) != 200
 content(response, "parsed")
 
-### Function to update a value in race_management ####
+# Function to update a value in race_management ####
 update_race_value <- function(name, value, url, api_key = NULL) {
   
   # Prepare the request body
@@ -73,9 +73,69 @@ now_ms <- function() {
   format(Sys.time(), "%Y-%m-%d %H:%M:%OS3")
 }
 
-l_data <- vector("list",1000)
+# Participant lookup by RFID ####
+library(tidyverse)
+library(httr)
 
-for (ii in 1:length(l_data)) {
-  l_data[[ii]] <- now_ms()
+# Function to lookup participant by RFID
+lookup_participant <- function(rfid) {
+  php_url <- paste0("http://", Sys.getenv("ZEIT_DB_HOST"), 
+                    "/zeitmessung/xampp/participant_lookup_by_RFID.php")
+  
+  # URL encode the RFID (colons need to be encoded)
+  encoded_rfid <- URLencode(rfid, reserved = TRUE)
+  
+  # Make the GET request
+  response <- GET(
+    url = paste0(php_url, "?rfid=", encoded_rfid),
+    timeout(10)
+  )
+  
+  # Check response
+  cat("Status code:", status_code(response), "\n")
+  cat("Content type:", headers(response)$`content-type`, "\n\n")
+  
+  # Parse response
+  if (status_code(response) == 200) {
+    result <- content(response, "parsed")
+    
+    if (result$status == "ok") {
+      return(result$data)
+    } else {
+      warning(paste("API error:", result$data$message))
+      return(NULL)
+    }
+  } else {
+    warning(paste("HTTP error:", status_code(response)))
+    
+    # Try to get error details
+    error_content <- content(response, "text")
+    cat("Error response:", error_content, "\n")
+    return(NULL)
+  }
 }
-l_data
+
+# Test with the RFID from your database
+test_rfids <- c(
+  "5A:01:CD:B2",  # Should find participant #5 (Jablonsk Schabi)
+  "5A:91:A7:AF",  # Should find participant #1 (Sager Lian)
+  "AA:BB:CC:DD"   # Should return "RFID not assigned"
+)
+
+for (rfid in test_rfids) {
+  cat("\n=== Testing RFID:", rfid, "===\n")
+  result <- lookup_participant(rfid)
+  
+  if (!is.null(result)) {
+    if (!is.null(result$participant)) {
+      cat("Found participant:\n")
+      cat("  Startnummer:", result$participant$Startnummer, "\n")
+      cat("  Name:", result$participant$Name, result$participant$Vorname, "\n")
+      cat("  On track:", result$on_track, "\n")
+      cat("  Allowed to lock:", result$allowed_to_lock, "\n")
+      cat("  Reason:", result$reason, "\n")
+    } else {
+      cat("RFID not assigned to any participant\n")
+    }
+  }
+}
