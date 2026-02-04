d23cfd67651e1ef24fedf157b343a7320d313379
Florian Wagner <slvwagner@gmail.com>
2026-02-04 18:36:45 +0100

    mc

--- configuration.R
+++ configuration.R
@@ -21,4 +21,38 @@ id <- df_spreadsheets|>
 df_config <- read_sheet(id)
 df_config
 
-source("source/Server_admin/Update_local_xampp_server.R")
+# # Copy all file to local xammp server
+# source("source/Server_admin/Update_local_xampp_server.R")
+# 
+# # Create named vector
+# env_vec <- df_config$value
+# names(env_vec) <- df_config$name
+# 
+# # set user variables 
+# set_windows_env <- function(name, value, scope = "user") {
+#   # scope: "user" (default) or "machine" (requires admin)
+#   
+#   value_escaped <- gsub('"', '""', value)  # Escape double quotes
+#   value_escaped <- gsub('%', '%%', value_escaped)  # Escape percent signs
+#   
+#   if (scope == "user") {
+#     cmd <- sprintf('setx "%s" "%s"', name, value_escaped)
+#   } else if (scope == "machine") {
+#     cmd <- sprintf('setx "%s" "%s" /M', name, value_escaped)
+#   } else {
+#     stop("scope must be 'user' or 'machine'")
+#   }
+#   
+#   result <- system(cmd, intern = TRUE, ignore.stderr = FALSE)
+#   return(result)
+# }
+# 
+# # Set them 
+# for (ii in 1:length(env_vec)) {
+#   set_windows_env(names(env_vec)[ii], env_vec[ii])
+# }
+# 
+# # check them 
+# Sys.getenv(names(env_vec))
+# 
+# 
