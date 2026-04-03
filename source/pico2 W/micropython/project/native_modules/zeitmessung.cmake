0af408fe6603c147ccbef2bb6953637dc00fa317
Florian Wagner <slvwagner@gmail.com>
2026-04-03 18:03:58 +0200

    New firmware including time v1

--- source/pico2 W/micropython/project/native_modules/zeitmessung.cmake
+++ source/pico2 W/micropython/project/native_modules/zeitmessung.cmake
@@ -5,9 +5,9 @@
 # This will be included in the build after user modules are processed
 string(TIMESTAMP ZEITMESSUNG_BUILD_TIMESTAMP "%Y-%m-%d %H:%M:%S")
 set(ZEITMESSUNG_BANNER "Raspberry Pi Pico 2 W [Zeitmessung FW] with RP2350 (built ${ZEITMESSUNG_BUILD_TIMESTAMP})")
+message(STATUS "Zeitmessung CMake: Setting MICROPY_BANNER_MACHINE = ${ZEITMESSUNG_BANNER}")
 
-if(TARGET firmware)
-    target_compile_definitions(firmware PRIVATE
-        MICROPY_BANNER_MACHINE="${ZEITMESSUNG_BANNER}"
-    )
-endif()
+# Use global add_compile_definitions since firmware target may not exist yet
+add_compile_definitions(MICROPY_BANNER_MACHINE="${ZEITMESSUNG_BANNER}")
+message(STATUS "Zeitmessung CMake: MICROPY_BANNER_MACHINE applied globally")
+
